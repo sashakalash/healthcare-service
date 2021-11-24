8c03ba4dab318a1a70996a4d29df424be2786f0d
Aleksandr Koloshtivin <aykolosh@mts.ru>
2021-11-24 20:26:36 +0300

    Добавлены тесты неиспользования alertService

--- src/test/java/MedicalServiceTest.java
+++ src/test/java/MedicalServiceTest.java
@@ -20,10 +20,12 @@ public class MedicalServiceTest {
             "Петров",
             LocalDate.of(1980, 11, 26),
             new HealthInfo(
-                    new BigDecimal("39.65"),
+                    new BigDecimal("36.65"),
                     new BloodPressure(120, 80)
             )
     );
+    private String patientId;
+    private final String MESSAGE = String.format("Warning, patient with id: %s, need help", PATIENT_INFO.getId());
 
     public MedicalServiceTest() {
         patientInfoRepository = Mockito.mock(PatientInfoRepository.class);
@@ -31,6 +33,7 @@ public class MedicalServiceTest {
                 .thenReturn(PATIENT_INFO.getId());
         Mockito.when(patientInfoRepository.getById(patientInfoRepository.add(PATIENT_INFO)))
                 .thenReturn(PATIENT_INFO);
+        patientId = patientInfoRepository.add(PATIENT_INFO);
 
         sendAlertService = Mockito.mock(SendAlertService.class);
         medicalServiceImpl = new MedicalServiceImpl(patientInfoRepository, sendAlertService);
@@ -38,22 +41,30 @@ public class MedicalServiceTest {
 
     @Test
     public void test_send_message_while_temperature_checking() {
-        PatientInfo patientInfo = PATIENT_INFO;
-        String message = String.format("Warning, patient with id: %s, need help", patientInfo.getId());System.out.printf("Warning, patient with id: %s, need help", patientInfo.getId());
-        String patientId = patientInfoRepository.add(PATIENT_INFO);
-        BigDecimal currentTemperature = new BigDecimal("37.9");
+        BigDecimal currentTemperature = new BigDecimal("34.20");
         medicalServiceImpl.checkTemperature(patientId, currentTemperature);
-        Mockito.verify(sendAlertService, Mockito.atLeastOnce()).send(message);
+        Mockito.verify(sendAlertService, Mockito.atLeastOnce()).send(MESSAGE);
     }
 
     @Test
     public void test_send_message_while_blood_checking() {
-        PatientInfo patientInfo = PATIENT_INFO;
-        String message = String.format("Warning, patient with id: %s, need help", patientInfo.getId());
-        String patientId = patientInfoRepository.add(PATIENT_INFO);
         BloodPressure currentPressure = new BloodPressure(60, 120);
         medicalServiceImpl.checkBloodPressure(patientId, currentPressure);
-        Mockito.verify(sendAlertService, Mockito.atLeastOnce()).send(message);
+        Mockito.verify(sendAlertService, Mockito.atLeastOnce()).send(MESSAGE);
+    }
+
+    @Test
+    public void test_unsend_message_while_normal_temp() {
+        BigDecimal currentTemperature = new BigDecimal("36.65");
+        medicalServiceImpl.checkTemperature(patientId, currentTemperature);
+        Mockito.verify(sendAlertService, Mockito.never()).send(MESSAGE);
+    }
+
+    @Test
+    public void test_unsend_message_while_normal_pressure() {
+        BloodPressure currentPressure = new BloodPressure(120, 80);
+        medicalServiceImpl.checkBloodPressure(patientId, currentPressure);
+        Mockito.verify(sendAlertService, Mockito.never()).send(MESSAGE);
     }
 
     @Test
